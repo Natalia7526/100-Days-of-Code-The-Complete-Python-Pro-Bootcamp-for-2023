a89e2603242b40361c59be1389aa96d1b514f8cf
Natalia <natalia.jasiczak96@gmail.com>
2023-12-21 18:31:22 +0100

    UI setup

--- Intermediate/Day_28/main.py
+++ Intermediate/Day_28/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,10 +10,41 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ---------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------ #
+
+
+
+# ---------------------------- COUNTDOWN MECHANISM -------------------- #
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.grid(column=1, row=1)
+
+
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "normal"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", font=(FONT_NAME, 10, "normal"), highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "normal"), highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "normal"))
+checkmark_label.grid(column=1, row=3)
+
+window.mainloop()
