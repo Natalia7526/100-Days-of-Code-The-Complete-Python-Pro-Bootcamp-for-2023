ed4ad01a9afa0b6555417976fcb718eeae80374e
Natalia <natalia.jasiczak96@gmail.com>
2024-01-07 22:09:41 +0100

    Step 4 - Save Your Progress

--- Intermediate/Day_31/main.py
+++ Intermediate/Day_31/main.py
@@ -1,50 +1,70 @@
-import random
 from tkinter import *
-import pandas as pd
+import pandas
+import random
 
-# ---------------------------- CONSTANTS ------------------------------ #
-GREEN = "#b1ddc6"
-TITLE_FONT = ("Ariel", 40, "italic")
-WORD_FONT = ("Ariel", 60, "bold")
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-# ---------------------------- DATA ----------------------------------- #
-data = pd.read_csv("data/french_words.csv")
-fr_dictionary = data.to_dict(orient="records")
-print(fr_dictionary)
-
-
-# ---------------------------- WORD GENERATION ------------------------ #
-def generate_word():
-    random_dict = random.choice(fr_dictionary)
-    random_word = random_dict['French']
-    canvas.itemconfig(word_text, text=random_word)
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    # print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-# ---------------------------- UI SETUP ------------------------------- #
-# Creating window
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50, bg=GREEN)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# The flash card
-canvas = Canvas(height=526, width=800)
-card_front = PhotoImage(file="images/card_front.png")
-card_back = PhotoImage(file="images/card_back.png")
-# position is half of height and width of canvas -> if image have to be in the center
-canvas.create_image(400, 263, image=card_front)
-title_text = canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
-word_text = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
-canvas.config(bg=GREEN, highlightthickness=0)
-canvas.grid(column=0, columnspan=2, row=0)
+flip_timer = window.after(3000, func=flip_card)
 
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-# Buttons
-right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=generate_word)
-right_button.grid(column=1, row=1)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=generate_word)
-wrong_button.grid(column=0, row=1)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
+
+
+
