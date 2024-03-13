743c454979bb029e05e12cfa921b758a07da7d8f
Natalia7526 <natalia.jasiczak96@gmail.com>
2024-03-13 16:48:31 +0100

    Day_37: HTTP Post Requests

---
+++ Intermediate/Day_37/main.py
@@ -0,0 +1,14 @@
+import requests
+
+pixela_endpoint = "https://pixe.la//v1/users"
+
+
+user_params = {
+    "token": "wurwirifslkls",
+    "username": "natalia7526",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
