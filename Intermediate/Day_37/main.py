3641521625a5e46b391d5f89f89270e5af11f06a
Natalia7526 <natalia.jasiczak96@gmail.com>
2024-03-13 17:20:39 +0100

    Advanced Authentication using an HTTP Header

--- Intermediate/Day_37/main.py
+++ Intermediate/Day_37/main.py
@@ -1,14 +1,34 @@
 import requests
 
+TOKEN = "wurwirifslkls"
+USERNAME = "natalia7526"
+
 pixela_endpoint = "https://pixe.la//v1/users"
 
-
 user_params = {
-    "token": "wurwirifslkls",
-    "username": "natalia7526",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "booktracker",
+    "name": "Reading Graph",
+    "unit": "pages",
+    "type": "int",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
+
